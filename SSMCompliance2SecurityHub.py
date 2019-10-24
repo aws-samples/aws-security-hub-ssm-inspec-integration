e636eb0cffdfaa26dcd9336293ff36e50e3e0e7c
Jonathan Rau <55855336+jonrau-at-aws@users.noreply.github.com>
2019-10-24 15:53:06 -0400

    Add files via upload

---
+++ SSMCompliance2SecurityHub.py
@@ -0,0 +1,241 @@
+ # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ # SPDX-License-Identifier: MIT-0
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ # software and associated documentation files (the "Software"), to deal in the Software
+ # without restriction, including without limitation the rights to use, copy, modify,
+ # merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ # permit persons to whom the Software is furnished to do so.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ # PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import json
+import boto3
+import datetime
+import uuid
+
+def lambda_handler(event, context):
+    # parse needed information from CloudWatch Event
+    noncompliantInstance = str(event['detail']['resource-id'])
+    cweEventType = str(event['detail-type'])
+    ssmManagedResourceArn = str(event['resources'][0])
+    cweSource = str(event['source'])
+    accountId = str(event['account'])
+    awsRegion = str(event['region'])
+    
+    # drop any SSM Compliance Items are are Compliant
+    complianceDetail = str(event['detail']['compliance-status'])
+    if complianceDetail != 'non_compliant':
+        print("Ignoring Compliant Resource!")
+        return 1
+    else:
+        print("Found non-compliant resource, proceeding")
+    
+    # at this point we identified a non-compliant instance
+    # import boto3 clients & resources for SSM, EC2 and Security Hub
+    ssm = boto3.client('ssm')
+    ec2client = boto3.client('ec2')
+    ec2 = boto3.resource('ec2')
+    securityhub = boto3.client('securityhub')
+    
+    # call SSM ListComplianceItems API, filter on non-compliant InSpec scans
+    try:
+        response = ssm.list_compliance_items(
+        Filters=[
+            {
+                'Key': 'ComplianceType',
+                'Values': [ 'Custom:InSpec' ],
+                'Type': 'EQUAL'
+            },
+            {
+                'Key': 'Status',
+                'Values': [ 'NON_COMPLIANT' ],
+                'Type': 'EQUAL'
+            }
+        ],
+        ResourceTypes=[ 'ManagedInstance' ],
+        ResourceIds=[ noncompliantInstance ],
+        MaxResults=1
+        )
+        print(response)
+    except Exception as e:
+        print(e)
+        raise
+
+    # pull out needed information from SSM ComplianceItems API to map to ASFF
+    ssmComplianceType = str(response['ComplianceItems'][0]['ComplianceType'])
+    inspecControlId = str(response['ComplianceItems'][0]['Id'])
+    inspecControlTitle = str(response['ComplianceItems'][0]['Title'])
+    ssmComplianceSeverity = str(response['ComplianceItems'][0]['Severity'])
+    ssmExecutionId = str(response['ComplianceItems'][0]['ExecutionSummary']['ExecutionId'])
+    ssmExecutionType = str(response['ComplianceItems'][0]['ExecutionSummary']['ExecutionType'])
+
+    # map ASFF Severity based on SSM Compliance Severty
+    # SSM = 'Severity': 'CRITICAL'|'HIGH'|'MEDIUM'|'LOW'|'INFORMATIONAL'|'UNSPECIFIED'
+    # SecHub = Allowed values are the following: PASSED, WARNING, FAILED, NOT_AVAILABLE
+    if ssmComplianceSeverity == 'UNSPECIFIED':
+        ssmASFFComplianceStatus = 'NOT_AVAILABLE'
+        ssmASFFProductSeverity = int(1)
+        ssmASFFProductNormalized = int(1)
+    elif ssmComplianceSeverity == 'INFORMATIONAL':
+        ssmASFFComplianceStatus = 'WARNING'
+        ssmASFFProductSeverity = int(1)
+        ssmASFFProductNormalized = int(1)
+    elif ssmComplianceSeverity == 'LOW':
+        ssmASFFComplianceStatus = 'FAILED'
+        ssmASFFProductSeverity = int(1)
+        ssmASFFProductNormalized = int(11)
+    elif ssmComplianceSeverity == 'MEDIUM':
+        ssmASFFComplianceStatus = 'FAILED'
+        ssmASFFProductSeverity = int(4)
+        ssmASFFProductNormalized = int(41)
+    elif ssmComplianceSeverity == 'HIGH':
+        ssmASFFComplianceStatus = 'FAILED'
+        ssmASFFProductSeverity = int(7)
+        ssmASFFProductNormalized = int(71)
+    elif ssmComplianceSeverity == 'CRITICAL':
+        ssmASFFComplianceStatus = 'FAILED'
+        ssmASFFProductSeverity = int(9)
+        ssmASFFProductNormalized = int(91)
+    else:
+        print("No Compliance Info Found!")
+    
+    print("Security Hub Severity identified as:" + " " + ssmComplianceSeverity)
+    
+    # use Ec2 Resource to pull out ASFF Ec2InstnaceInfo for mapping
+    try:
+        ec2InstanceIntel = ec2.Instance(noncompliantInstance)
+        ec2Type = ec2InstanceIntel.instance_type
+        ec2ImageId = ec2InstanceIntel.image_id
+        ec2PubIPv4 = ec2InstanceIntel.public_ip_address
+        ec2PrivIPv4 = ec2InstanceIntel.private_ip_address
+        ec2KeyName = ec2InstanceIntel.key_name
+        ec2InstanceProfile = ec2InstanceIntel.iam_instance_profile
+        ec2SubnetInfo = ec2VpcInfo = ec2InstanceIntel.subnet_id
+        ec2VpcInfo = ec2InstanceIntel.vpc_id
+        ec2SecurityGroup = ec2InstanceIntel.security_groups
+        securitygroupName = str(ec2SecurityGroup[0]['GroupName'])
+        securitygroupId = str(ec2SecurityGroup[0]['GroupId'])
+        print("EC2 data enrichment complete")
+    except Exception as e:
+        print(e)
+        raise
+
+    try:
+        findNACL = ec2client.describe_network_acls(
+        Filters=[
+            {
+                'Name' :'association.subnet-id',
+                'Values': [ ec2SubnetInfo ]
+            }
+        ],
+        DryRun=False
+        )
+        ec2NaclInfo = str(findNACL['NetworkAcls'][0]['Associations'][0]['NetworkAclId'])
+        print(findNACL)
+        print(noncompliantInstance + " " + "Network ACL identified")
+    except Exception as e:
+        print(e)
+        raise
+
+    # call SSM DescribeInstanceInformation API, pull out SSM Agent telemetry to map to ASFF Resource.Other field
+    try:
+        describeInstanceInformation = ssm.describe_instance_information(
+        InstanceInformationFilterList=[
+            {
+                'key': 'InstanceIds',
+                'valueSet': [ noncompliantInstance ]
+            }
+        ],
+        MaxResults=20
+        )
+        ssmAgentVersion = str(describeInstanceInformation['InstanceInformationList'][0]['AgentVersion'])
+        ssmPlatformType = str(describeInstanceInformation['InstanceInformationList'][0]['PlatformType'])
+        ssmPlatformName = str(describeInstanceInformation['InstanceInformationList'][0]['PlatformName'])
+        ssmPlatformVersion = str(describeInstanceInformation['InstanceInformationList'][0]['PlatformVersion'])
+        print(describeInstanceInformation)
+        print("SSM data enrichment complete")
+    except Exception as e:
+        print(e)
+    
+    # ISO Time
+    iso8061Time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    # ASFF BIF Id
+    asffID = str(uuid.uuid4())
+
+    # map SSM, EC2 and CloudWatch Information into ASFF and send to Security Hub
+    try:
+        response = securityhub.batch_import_findings(
+        Findings=[
+            {
+                'SchemaVersion': '2018-10-08',
+                'Id': asffID,
+                'ProductArn': 'arn:aws:securityhub:' + awsRegion + ':' + accountId + ':product/' + accountId + '/default',
+                'GeneratorId': ssmExecutionId,
+                'AwsAccountId': accountId,
+                'Types': [ 'Software and Configuration Checks' ],
+                'FirstObservedAt': iso8061Time,
+                'UpdatedAt': iso8061Time,
+                'CreatedAt': iso8061Time,
+                'Severity': {
+                    'Product': ssmASFFProductSeverity,
+                    'Normalized': ssmASFFProductNormalized
+                },
+                'Confidence': 99,
+                'Title': 'EC2 Instance' + ' ' + noncompliantInstance + ' ' + 'has failed SSM Compliance Type' + ' ' + ssmComplianceType + ' ' + 'check for:' + ' ' + inspecControlTitle,
+                'Description': 'EC2 Instance' + ' ' + noncompliantInstance + ' ' + 'has failed SSM Compliance Type' + ' ' + ssmComplianceType + ' ' + 'check for:' + ' ' + inspecControlTitle + ' ' + 'ID is:' + ' ' + inspecControlId,
+                'ProductFields': {
+                    'ProviderName': 'AWS Systems Manager Compliance'
+                },
+                'UserDefinedFields': {
+                    'CloudWatch Detail Type': cweEventType,
+                    'CloudWatch Source Detail': cweSource,
+                    'InSpec Gemfile Title': inspecControlId,
+                    'SSM Execution Type': ssmExecutionType,
+                    'SSM Managed Instance ARN': ssmManagedResourceArn
+                },
+                'Resources': [
+                    {
+                        'Type': 'AwsEc2Instance',
+                        'Id': 'arn:aws:ec2:' + awsRegion + ':' + accountId + ':' + 'instance/' + noncompliantInstance,
+                        'Partition': 'aws',
+                        'Region': awsRegion,
+                        'Details': {
+                            'AwsEc2Instance': {
+                                'Type': ec2Type,
+                                'ImageId': ec2ImageId,
+                                'IpV4Addresses': [ ec2PrivIPv4, ec2PubIPv4 ],
+                                'KeyName': ec2KeyName,
+                                'IamInstanceProfileArn': ec2InstanceProfile['Arn'],
+                                'VpcId': ec2VpcInfo,
+                                'SubnetId': ec2SubnetInfo
+                            },
+                            "Other": { 
+                                "instanceID-nonArn" : noncompliantInstance,
+                                "networkACLId" : ec2NaclInfo,
+                                "securityGroupName" : securitygroupName,
+                                "securityGroupId" : securitygroupId,
+                                "ssmAgentVersion" : ssmAgentVersion,
+                                "ssmPlatformType" : ssmPlatformType,
+                                "ssmPlatformName" : ssmPlatformName,
+                                "ssmPlatformVersion" : ssmPlatformVersion
+                            }
+                        }
+                    },
+                ],
+                'Compliance': {'Status': ssmASFFComplianceStatus},
+                'VerificationState': 'TRUE_POSITIVE',
+                'WorkflowState': 'NEW',
+                'RecordState': 'ACTIVE'
+            }
+        ]
+        )
+        print(response)
+    except Exception as e:
+        print(e)
+        raise
